d026196e58c68a72b7e81756b777bae1e3f10829
Leon Suarez <leonsuarez24@gmail.com>
2022-12-31 16:53:41 -0500

    agregando inputs y botones

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("asdasd");
+const encriptarButton = document.getElementById("encriptar")
+const desencriptarButton = document.getElementById("desencriptar")
+const copiarButton = document.getElementById("copiar")
+
+const inputText =  document.getElementById("text-input")
+
+
+encriptarButton.addEventListener("click", () => {
+  let contenido = inputText.value
+  console.log(contenido);
+})
+
+desencriptarButton.addEventListener("click", () => {
+  let contenido = inputText.value
+  console.log(contenido);
+})
